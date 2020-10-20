4e72fa3b3ac303133dc63adea609057e5b97f515
AlejandroOliver12 <71462239+AlejandroOliver12@users.noreply.github.com>
2020-10-20 10:53:11 -0700

    Update ejercicio3.scala

--- Unit_1/Practices/Practice_4/ejercicio3.scala
+++ Unit_1/Practices/Practice_4/ejercicio3.scala
@@ -1,1 +1,20 @@
-
+//Exercise3
+//define fib
+//Declare 2 variables type Int
+def fib(n:Int):Int = {
+    var a:Int = 0
+    var b: Int = 1
+   
+    //Use for Cicle
+    
+    for (k<- Range(0,n)){
+    //Declare variable C type int
+       var c :Int = b+a
+        a = b
+        b = c
+    }
+     //Return a   
+     return a
+    }
+//result
+fib(9)
