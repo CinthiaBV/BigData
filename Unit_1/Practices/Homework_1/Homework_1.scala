f1d94a58002d1bdf874dc61cda5bece8ec6559df
CinthiaBV <cinthia.vargas18@tectijuana.edu.mx>
2020-10-22 13:01:37 -0700

    Done Investigation_1, Homework_1, Practice_GitHub

---
+++ Unit_1/Practices/Homework_1/Homework_1.scala
@@ -0,0 +1,63 @@
+Content
+
+What is Pearson's correlation coefficient? 3
+How the Pearson 3 Correlation Coefficient is Calculated
+Interpretation of the Karl Pearson correlation coefficient 3
+Advantages and disadvantages of the Pearson 4 correlation coefficient
+Bibliography 5
+
+
+
+
+What is Pearson's correlation coefficient?
+
+Pearson's correlation coefficient is a test that measures the statistical relationship between two continuous variables. 
+If the association between the elements is not linear, then the coefficient is not adequately represented.
+The correlation coefficient can take a range of values ​​from +1 to -1. A value of 0 indicates that there is no association 
+between the two variables. A value greater than 0 indicates a positive association. That is, as the value of one variable 
+increases, so does the value of the other. A value less than 0 indicates a negative association; that is, as the value of 
+one variable increases, the value of the other decreases.
+To carry out the Pearson correlation it is necessary to fulfill the following:
+• The measurement scale must be an interval or ratio scale.
+• The variables must be approximately distributed.
+• The association must be linear.
+• There should be no outliers in the data.
+How Pearson's Correlation Coefficient Is Calculated
+
+The Pearson correlation coefficient formula is as follows:
+Where:
+"X" is equal to variable number one, "y" belongs to variable number two, "zx" is the standard deviation of variable one, "zy"
+ is the standard deviation of variable two and "N" is is number of data.
+I recommend you read this article about what correlational research is.
+
+Interpretation of the Karl Pearson correlation coefficient
+
+Pearson's correlation coefficient has the objective of indicating how associated two variables are with each other, therefore:
+Correlation less than zero: If the correlation is less than zero, it means that it is negative, that is, the variables are inversely related.
+When the value of some variable is high, the value of the other variable is low. The closer it is to -1, the clearer the extreme covariance. 
+If the coefficient is equal to -1, we mean a perfect negative correlation.
+
+
+Correlation greater than zero: If the correlation is equal to +1 it means that it is perfect positive. In this case it means that the correlation
+ is positive, that is, that the variables are directly correlated.
+When the value of one variable is high, the value of the other is also high, the same happens when they are low. If it is close to +1, the coefficient
+ will be the covariance.
+Correlation equal to zero: When the correlation is equal to zero it means that it is not possible to determine some sense of covariation. However,
+ it does not mean that there is no nonlinear relationship between the variables.
+When the variables are independent, it means that they are correlated, but this does not mean that the result is true.
+Advantages and disadvantages of the Pearson correlation coefficient
+
+Among the main advantages of the Karl Pearson correlation coefficient are:
+• The value is independent of whatever unit is used to measure the variables.
+• If the sample is large, the accuracy of the estimate is more likely.
+Some of the disadvantages of the correlation coefficient are:
+• It is necessary both variables be measured at a continuous quantitative level.
+• The distribution of the variables must be similar to the normal curve.
+
+
+
+Bibliography
+
+Pearson's correlation. (2012). Pearson correlation. https://www.questionpro.com/blog/es/pearson-correlation-coefficient/
+Ruiz Mitjana, L. (2020, October 16). Pearson's correlation coefficient: what it is and how it is used. Psychology and Mind.
+ https://psicologiaymente.com/miscelanea/coeficiencia-correlacion-pearson
---
+++ Unit_1/Practices/Investigation_1/PairCoding.scala
@@ -0,0 +1,63 @@
+Introduction
+
+Git is an open source version specific control system created by Linus Torvalds in 2005.
+
+Specifically, Git is a distributed version control system, which means that the entire code base and its history 
+are available on every developer's computer, allowing easy access to branching and merging.
+
+GitHub is a non-profit company that offers a hosting service for repositories stored in the cloud. Essentially, it makes it 
+easier for individuals and teams to use Git as the control and collaboration version. The GitHub interface is fairly easy to 
+use for the novice developer who wants to take advantage of Git. Without GitHub, using Git generally requires a bit more tech savvy and command line usage.
+
+Developing
+
+The activities to be followed in practice were:
+
+1. Create an account on github.com if you don't have one, if you already have one then we're fine
+
+ 
+2. Configure your github account with SHH key, so as not to put our username and password every time
+ 
+
+3. Create a new repository called pratica_git_flow
+
+ 
+
+
+4. Create a default README.md
+
+ 
+
+5. Clone our PC
+
+
+ 
+
+6. Create a branch called development and make this branch the main one, this means that it will not be master
+ 
+
+ 
+
+
+7. Make a change in the development branch in the README.md file could be "This is the development branch"
+ 
+
+
+8. Make a commit in the development branch
+ 
+
+
+9. Push the development branch
+ 
+10. Create a branch called features
+
+ 
+11. Make a change in the features branch in the README.md file can be "This is the features branch"
+
+ 
+
+conclusion
+
+It is the first time that I use the Git Hub tool, so I had to watch some videos to know how it worked,
+ as well as making a mistake when creating the SSH key and its respective passwords, however, I think it's all a matter of practice,
+  to be able to memorize the commands and thus everything is easier
